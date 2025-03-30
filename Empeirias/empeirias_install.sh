4fdb9637a96f063ff334eea824ef28861e31d2eb
OneNov0209 <95606017+OneNov0209@users.noreply.github.com>
2025-03-30 08:31:56 +0800

    Add files via upload

---
+++ Empeirias/empeirias_install.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+source <(curl -s https://raw.githubusercontent.com/itrocket-team/testnet_guides/main/utils/common.sh)
+
+# Replace the original logo with ONENOV logo
+function printLogo() {
+  echo -e "\033[0;36m"
+  echo ' ██████╗ ███╗   ██╗███████╗███╗   ██╗ ██████╗ ██╗   ██╗'
+  echo '██╔═══██╗████╗  ██║██╔════╝████╗  ██║██╔═══██╗██║   ██║'
+  echo '██║   ██║██╔██╗ ██║█████╗  ██╔██╗ ██║██║   ██║██║   ██║'
+  echo '██║   ██║██║╚██╗██║██╔══╝  ██║╚██╗██║██║   ██║╚██╗ ██╔╝'
+  echo '╚██████╔╝██║ ╚████║███████╗██║ ╚████║╚██████╔╝ ╚████╔╝'
+  echo ' ╚═════╝ ╚═╝  ╚═══╝╚══════╝╚═╝  ╚═══╝ ╚═════╝   ╚═══╝'
+  echo -e "\033[0m"
+}
+
+printLogo
+
+read -p "Enter WALLET name:" WALLET
+echo 'export WALLET='$WALLET
+read -p "Enter your MONIKER :" MONIKER
+echo 'export MONIKER='$MONIKER
+read -p "Enter your PORT (for example 17, default port=26):" PORT
+echo 'export PORT='$PORT
+
+# set vars
+echo "export WALLET="$WALLET"" >> $HOME/.bash_profile
+echo "export MONIKER="$MONIKER"" >> $HOME/.bash_profile
+echo "export EMPED_CHAIN_ID="empe-testnet-2"" >> $HOME/.bash_profile
+echo "export EMPED_PORT="$PORT"" >> $HOME/.bash_profile
+source $HOME/.bash_profile
+
+printLine
+echo -e "Moniker:        \e[1m\e[32m$MONIKER\e[0m"
+echo -e "Wallet:         \e[1m\e[32m$WALLET\e[0m"
+echo -e "Chain id:       \e[1m\e[32m$EMPED_CHAIN_ID\e[0m"
+echo -e "Node custom port:  \e[1m\e[32m$EMPED_PORT\e[0m"
+printLine
+sleep 1
+
+printGreen "1. Installing go..." && sleep 1
+# install go, if needed
+cd $HOME
+VER="1.22.3"
+wget "https://golang.org/dl/go$VER.linux-amd64.tar.gz"
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf "go$VER.linux-amd64.tar.gz"
+rm "go$VER.linux-amd64.tar.gz"
+[ ! -f ~/.bash_profile ] && touch ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:~/go/bin" >> ~/.bash_profile
+source $HOME/.bash_profile
+[ ! -d ~/go/bin ] && mkdir -p ~/go/bin
+
+echo $(go version) && sleep 1
+
+source <(curl -s https://raw.githubusercontent.com/itrocket-team/testnet_guides/main/utils/dependencies_install)
+
+printGreen "4. Installing binary..." && sleep 1
+# download binary
+cd $HOME
+rm -rf bin
+mkdir bin
+cd $HOME/bin
+curl -LO https://github.com/empe-io/empe-chain-releases/raw/master/v0.3.0/emped_v0.3.0_linux_amd64.tar.gz
+tar -xvf emped_v0.2.2_linux_amd64.tar.gz
+chmod +x $HOME/bin/emped
+mv $HOME/bin/emped ~/go/bin
+
+printGreen "5. Configuring and init app..." && sleep 1
+# config and init app
+emped config node tcp://localhost:${EMPED_PORT}657
+emped config keyring-backend os
+emped config chain-id empe-testnet-2
+emped init $MONIKER --chain-id empe-testnet-2
+sleep 1
+echo done
+
+printGreen "6. Downloading genesis and addrbook..." && sleep 1
+# download genesis and addrbook
+wget -O $HOME/.empe-chain/config/genesis.json https://server-5.itrocket.net/testnet/empeiria/genesis.json
+wget -O $HOME/.empe-chain/config/addrbook.json  https://server-5.itrocket.net/testnet/empeiria/addrbook.json
+sleep 1
+echo done
+
+printGreen "7. Adding seeds, peers, configuring custom ports, pruning, minimum gas price..." && sleep 1
+# set seeds and peers
+SEEDS="20ca5fc4882e6f975ad02d106da8af9c4a5ac6de@empeiria-testnet-seed.itrocket.net:28656"
+PEERS="03aa072f917ed1b79a14ea2cc660bc3bac787e82@empeiria-testnet-peer.itrocket.net:28656,106b4f4e333bd04d2b93768dace23bae12ebc1b7@65.109.112.148:21156,a9cf0ffdef421d1f4f4a3e1573800f4ee6529773@136.243.13.36:29056,e058f20874c7ddf7d8dc8a6200ff6c7ee66098ba@65.109.93.124:29056,af1bae5ad434fc2188a1ef9bed23398492826896@193.34.212.80:11156,066463be254c41ce3e10d9c0caa5988cdd8c212d@31.220.84.240:26656,2db322b41d26559476f929fda51bce06c3db8ba4@65.109.24.155:11256,38ca15d129e9f02ff4164649f1e8ba1325237e7f@194.163.145.153:26656,fec4ba35a0c58c29a101d728a5008370ac6fe7ed@116.202.150.231:28656,78f766310a83b6670023169b93f01d140566db79@65.109.83.40:29056,d4e183a6637a8e0844b6c5cecc55a440891b8275@[2a01:4f9:3051:19c2::2]:14056"
+sed -i -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*seeds *=.*/seeds = \"$SEEDS\"/}" \
+       -e "/^\[p2p\]/,/^\[/{s/^[[:space:]]*persistent_peers *=.*/persistent_peers = \"$PEERS\"/}" \
+       $HOME/.empe-chain/config/config.toml
+
+# set custom ports in app.toml
+sed -i.bak -e "s%:1317%:${EMPED_PORT}317%g;
+s%:8080%:${EMPED_PORT}080%g;
+s%:9090%:${EMPED_PORT}090%g;
+s%:9091%:${EMPED_PORT}091%g;
+s%:8545%:${EMPED_PORT}545%g;
+s%:8546%:${EMPED_PORT}546%g;
+s%:6065%:${EMPED_PORT}065%g" $HOME/.empe-chain/config/app.toml
+
+
+# set custom ports in config.toml file
+sed -i.bak -e "s%:26658%:${EMPED_PORT}658%g;
+s%:26657%:${EMPED_PORT}657%g;
+s%:6060%:${EMPED_PORT}060%g;
+s%:26656%:${EMPED_PORT}656%g;
+s%^external_address = \"\"%external_address = \"$(wget -qO- eth0.me):${EMPED_PORT}656\"%;
+s%:26660%:${EMPED_PORT}660%g" $HOME/.empe-chain/config/config.toml
+
+# config pruning
+sed -i -e "s/^pruning *=.*/pruning = \"custom\"/" $HOME/.empe-chain/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/" $HOME/.empe-chain/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"50\"/" $HOME/.empe-chain/config/app.toml
+
+# set minimum gas price, enable prometheus and disable indexing
+sed -i 's|minimum-gas-prices =.*|minimum-gas-prices = "0.0001uempe"|g' $HOME/.empe-chain/config/app.toml
+sed -i -e "s/prometheus = false/prometheus = true/" $HOME/.empe-chain/config/config.toml
+sed -i -e "s/^indexer *=.*/indexer = \"null\"/" $HOME/.empe-chain/config/config.toml
+sleep 1
+echo done
+
+# create service file
+sudo tee /etc/systemd/system/emped.service > /dev/null <<EOF
+[Unit]
+Description=empeiria node
+After=network-online.target
+[Service]
+User=$USER
+WorkingDirectory=$HOME/.empe-chain
+ExecStart=$(which emped) start --home $HOME/.empe-chain
+Restart=on-failure
+RestartSec=5
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+printGreen "8. Downloading snapshot and starting node..." && sleep 1
+# reset and download snapshot
+emped tendermint unsafe-reset-all --home $HOME/.empe-chain
+if curl -s --head curl https://server-5.itrocket.net/testnet/empeiria/empeiria_2025-03-29_4241356_snap.tar.lz4 | head -n 1 | grep "200" > /dev/null; then
+  curl https://server-5.itrocket.net/testnet/empeiria/empeiria_2025-03-29_4241356_snap.tar.lz4 | lz4 -dc - | tar -xf - -C $HOME/.empe-chain
+    else
+  echo "no snapshot found"
+fi
+
+# enable and start service
+sudo systemctl daemon-reload
+sudo systemctl enable emped
+sudo systemctl restart emped && sudo journalctl -u emped -f -o cat
